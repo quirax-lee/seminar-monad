5e86672b57af26d829139596fdca2d6553c02b53
Quirax Lee <qrlee@devguru.co.kr>
2023-06-08 10:16:57 +0900

    Functor를 확장한 형태로 Monad 수정

--- monad/6.ts
+++ monad/6.ts
@@ -1,22 +1,24 @@
-class Monad<T> {
-    private value: T | undefined;
+class Functor<T> {
+    protected value: T | undefined;
 
     public constructor(value?: T) {
         this.value = value;
     }
 
-    public map<U>(fn: (n: T) => U): Monad<U> {
-        if(this.value === undefined) return new Monad<U>(undefined);
-        else return new Monad<U>(fn(this.value));
-    }
-
-    public flatMap<U>(fn: (n: T) => Monad<U>): Monad<U> {
-        if(this.value === undefined) return new Monad<U>(undefined);
-        else return new Monad<U>(fn(this.value).value);
+    public map<U>(fn: (n: T) => U): Functor<U> {
+        if(this.value === undefined) return new Functor<U>(undefined);
+        else return new Functor<U>(fn(this.value));
     }
 
     public get(): T|undefined {
         return this.value;
+    }
+}
+
+class Monad<T> extends Functor<T> {
+    public flatMap<U>(fn: (n: T) => Monad<U>): Monad<U> {
+        if(this.value === undefined) return new Monad<U>(undefined);
+        else return new Monad<U>(fn(this.value).value);
     }
 }
 
